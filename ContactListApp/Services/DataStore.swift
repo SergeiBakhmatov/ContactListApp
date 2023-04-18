42f55e8a7f5a7194c046cc40b28bcd3d0df76987
Sergei Bakhmatov <94bahmatov@gmail.com>
2023-04-18 13:48:06 +0500

    editing DataStore

--- ContactListApp/Services/DataStore.swift
+++ ContactListApp/Services/DataStore.swift
@@ -7,51 +7,51 @@
 
 class DataStore {
     let names = [
-    "Jhon",
-    "Sharon",
-    "Aaron",
-    "Steven",
-    "Nicola",
-    "Ted",
-    "Bruce",
-    "Carl",
-    "Alan",
-    "Sergei"
+        "Jhon",
+        "Sharon",
+        "Aaron",
+        "Steven",
+        "Nicola",
+        "Ted",
+        "Bruce",
+        "Carl",
+        "Alan",
+        "Sergei"
     ]
     let surnames = [
-    "Murphy",
-    "Jankin",
-    "Williams",
-    "Black",
-    "Robertson",
-    "Butler",
-    "Isaacson",
-    "Smith",
-    "Pennywoth",
-    "Bakhmatov"
+        "Murphy",
+        "Jankin",
+        "Williams",
+        "Black",
+        "Robertson",
+        "Butler",
+        "Isaacson",
+        "Smith",
+        "Pennywoth",
+        "Bakhmatov"
     ]
     let phoneNumbers = [
-    "999333888",
-    "123123123",
-    "456456456",
-    "789789789",
-    "111222333",
-    "444555666",
-    "777888999",
-    "223344556",
-    "192837465",
-    "777777777"
+        "999333888",
+        "123123123",
+        "456456456",
+        "789789789",
+        "111222333",
+        "444555666",
+        "777888999",
+        "223344556",
+        "192837465",
+        "777777777"
     ]
     let emails = [
-    "asd@mail.com",
-    "dsa@mail.com",
-    "qwe@mail.com",
-    "ewq@mail.com",
-    "zxc@mail.com",
-    "cxz@mail.com",
-    "rty@mail.com",
-    "ytr@mail.com",
-    "fgh@mail.com",
-    "hgf@mail.com"
+        "asd@mail.com",
+        "dsa@mail.com",
+        "qwe@mail.com",
+        "ewq@mail.com",
+        "zxc@mail.com",
+        "cxz@mail.com",
+        "rty@mail.com",
+        "ytr@mail.com",
+        "fgh@mail.com",
+        "hgf@mail.com"
     ]
 }
