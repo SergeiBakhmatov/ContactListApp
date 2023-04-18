89ceed5f70ad984717dde515e1800a05b4a3a66b
Sergei Bakhmatov <94bahmatov@gmail.com>
2023-04-18 13:46:41 +0500

    Add DataStore

---
+++ ContactListApp/Services/DataStore.swift
@@ -0,0 +1,57 @@
+//
+//  DataStore.swift
+//  ContactListApp
+//
+//  Created by Sergei Bakhmatov on 18.04.2023.
+//
+
+class DataStore {
+    let names = [
+    "Jhon",
+    "Sharon",
+    "Aaron",
+    "Steven",
+    "Nicola",
+    "Ted",
+    "Bruce",
+    "Carl",
+    "Alan",
+    "Sergei"
+    ]
+    let surnames = [
+    "Murphy",
+    "Jankin",
+    "Williams",
+    "Black",
+    "Robertson",
+    "Butler",
+    "Isaacson",
+    "Smith",
+    "Pennywoth",
+    "Bakhmatov"
+    ]
+    let phoneNumbers = [
+    "999333888",
+    "123123123",
+    "456456456",
+    "789789789",
+    "111222333",
+    "444555666",
+    "777888999",
+    "223344556",
+    "192837465",
+    "777777777"
+    ]
+    let emails = [
+    "asd@mail.com",
+    "dsa@mail.com",
+    "qwe@mail.com",
+    "ewq@mail.com",
+    "zxc@mail.com",
+    "cxz@mail.com",
+    "rty@mail.com",
+    "ytr@mail.com",
+    "fgh@mail.com",
+    "hgf@mail.com"
+    ]
+}
